9d4d3de7ff1445608ecda90f85756f68a49b26ac
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-01 09:33:17 +0000

    2nd test passes: application returns capitalised single letter words as it was inputted

--- jasmine/spec/middleLetterSpec.js
+++ jasmine/spec/middleLetterSpec.js
@@ -4,4 +4,8 @@ describe("middleLetter", () => {
     word = "a";
     expect(middleLetter(word)).toEqual("a");
   })
+  it("returns a single capitalised letter that is inputted", () => {
+    word = "A";
+    expect(middleLetter(word)).toEqual("A");
+  })
 })
--- jasmine/src/middleLetter.js
+++ jasmine/src/middleLetter.js
@@ -1,3 +1,3 @@
-function middleLetter() {
-  return "a";
+function middleLetter(word) {
+  return word;
 }
