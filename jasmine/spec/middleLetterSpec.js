b912f7a7f888b04babcb1d7ddead33e5e05b525b
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-01 09:31:36 +0000

    First test passes: Application takes single word input and returns it as it is

---
+++ jasmine/spec/middleLetterSpec.js
@@ -0,0 +1,7 @@
+describe("middleLetter", () => {
+  var word;
+  it("returns a single letter that is inputted", () => {
+    word = "a";
+    expect(middleLetter(word)).toEqual("a");
+  })
+})
---
+++ jasmine/src/middleLetter.js
@@ -0,0 +1,3 @@
+function middleLetter() {
+  return "a";
+}
